7d9d42a44e61daf8313701100e743e9fa4272920
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2014-03-11 13:32:02 +0900

    add youtube player class

---
+++ app/scripts/media/video/YouTubeEmbed.js
@@ -0,0 +1,91 @@
+(function(){
+    'use strict';
+
+    /**
+     * @class Global.media.video.YoutubeEmbed
+     */
+    Global.define('Global.media.video.YoutubeEmbed',{
+
+        extend: Global.ObservableClass,
+
+        eventName: {
+            loadSdk   : 'loadsdk',
+            loadPlayer: 'loadplayer'
+        },
+
+        $elm: null,
+
+        sdkId: 'g-youtube-embed',
+
+        sdkSrc: 'https://www.youtube.com/iframe_api',
+
+        inst: null,
+
+        param: null,
+
+        init: function(config){
+            this.listeners = {};
+            this._super(config);
+        },
+
+        initSdk: function(){
+            if(this._hasSdk()){
+                this.embed();
+                return;
+            }
+            this._appendSdk();
+        },
+
+        _appendSdk: function(){
+            window.onYouTubeIframeAPIReady = Global.Functions.bind(this._onLoadSdk, this);
+            var $script = this._getSdkCode();
+            $(document.body).append($script);
+        },
+
+        _onLoadSdk: function(){
+            this.dispatchEvent(this.getEventName().loadSdk, {currentTarget: this});
+            this.embed();
+        },
+
+        _hasSdk: function(){
+            var $sdk = $('#' + this._getSdkId());
+            return ($sdk.length > 0) && window.YT;
+        },
+
+        _getSdkCode: function(){
+            var $script = $('script'),
+                id = this.getSdkId(),
+                src = this.getSdkSrc();
+            $script.prop('id', id);
+            $script.prop('src', src);
+            return $script;
+        },
+
+        embed: function(){
+            var param = this._getParam(),
+                inst = this._createEmbedInstance(param);
+            this.inst = inst;
+        },
+
+        _createEmbedInstance: function(param){
+            return new window.YT.Player('player',param);
+        },
+
+        _getParam: function(){
+            var param = this.getParam(),
+                add = {
+                    events: {
+                        'onReady': Global.Functions.bind(this._onReadyVideo, this)
+                    }
+                };
+            return $.exted(true, {}, param, add);
+        },
+
+        _onReadyVideo: function(/*e*/){
+            this.dispatchEvent(this.getEventName().loadPlayer, {currentTarget: this});
+            // override as you like
+            //e.playVideo();
+        }
+
+    });
+})();
